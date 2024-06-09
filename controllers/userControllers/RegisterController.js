d91d28fbbba60a3eb04f1485f139bd99e978ae37
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-09 16:35:58 +0530

    changed imported location

--- controllers/userControllers/RegisterController.js
+++ controllers/userControllers/RegisterController.js
@@ -1,5 +1,5 @@
 
-import { User } from "../../models/user.models.js";
+import { User } from "../../models/user.model.js";
 import { handleResponse } from "../../utils/HnadleResponse.js";
 
 //@desc Add User
