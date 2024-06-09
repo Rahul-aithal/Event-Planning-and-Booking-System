f4cf0f0a651bad67e730af2662ff8b7181959b3f
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-09 16:30:05 +0530

    Event Model created

--- models/event.model.js
+++ models/event.model.js
@@ -1,26 +1,37 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
 
+const eventSchema = mongoose.Schema( {
+    owner: {
+        type: Object,
+        require:true,
+        unique:ture
+    },
+    title: {
+        type: String,    //One who Books for event
+        require:true,
+        unique:ture
+    },
+    description:{
+        type:String,
+    },
+    date:{
+        type:String,
+        require:true,
+    },
+    location:{
+        type:String,
+        require:true,
+    },
+    availableSeats:{
+        type:Number,
+        require:true
+    }
 
-const eventSchema = new mongoose.Schema(
-    {
-        eventName: {
-            type: String,
-            required: true
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        },
-        booker: {
-            type: Schema.Types.ObjectId,    //One who Books for event
-            ref: "User"
-        },
+
+}, { timestamps: true });
 
 
-
-    }, { timestamps: true }
-);
-
+eventSchema.index({"owner.id":1,title:1,date:1,location:1},{unique:true});
 
 export const Event = mongoose.model("Event", eventSchema);
