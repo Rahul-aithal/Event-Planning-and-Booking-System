f50a35840b8946f2f633668ebc5489d8a4a1af94
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-09 16:37:52 +0530

    log router and user router merged

--- Routers/user.router.js
+++ Routers/user.router.js
@@ -3,21 +3,33 @@ import { GetUsers, Getuser } from "../controllers/userControllers/GetUsersContro
 import { register } from "../controllers/userControllers/RegisterController.js";
 import { DelteUser } from "../controllers/userControllers/DeleteController.js";
 import { UpdateEmail, UpdateName, UpdatePassword } from "../controllers/userControllers/UpadteController.js";
-
+import { verifyToken } from "../middlewares/verifyJWT.middleware.js";
+import { LogOut, Login, refreshAccessToken } from "../controllers/userControllers/LogController.js";
 
 const router = Router()
 
+router.use(verifyToken);
 
-router.post('/all', GetUsers)
-router.get('/', Getuser)
+//Get Users
+router.post('/all', GetUsers);
+router.get('/', Getuser);
 
+//Regiseter new User
 router.post("/register", register)
 
+//Log in or out user
+router.post("/login", Login);
+router.post("/logout/", verifyToken, LogOut);
 
-router.put("/username", UpdateName);
+//Givn new Access Token based on refrsh Token
+router.post("/refreshaccesstoken",refreshAccessToken);
+
+//Upadate user details
+router.put("/username",UpdateName);
 router.put("/email", UpdateEmail);
-router.put("/password", UpdatePassword);
+router.put("/password",UpdatePassword);
 
+//Delete User
 router.delete("/deleteuser", DelteUser);
 
 export default router;
