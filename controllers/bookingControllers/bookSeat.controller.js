ac1fff5fb9f72adea959d93d7c5c6783789a00c8
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-11 16:45:03 +0530

    chaged the seats values

--- controllers/bookingControllers/bookSeat.controller.js
+++ controllers/bookingControllers/bookSeat.controller.js
@@ -6,11 +6,12 @@ import { Booking } from "../../models/booking.model.js";
 
 export const bookSeat = async (req, res, next) => {
     const user = req.user;
-    const { title, date, location } = req.body;
+    const { owner,title, date, location,bookedSeats } = req.body;
     const eventDetails = { title, date, location }
     if (!(user && eventDetails)) return handleResponse(res, 404, {}, new Error("Data missing"), next);
     try {
         const event = await Event.findOne({
+            owner,
             title,
             date,
             location
@@ -19,14 +20,15 @@ export const bookSeat = async (req, res, next) => {
 
         const booking = new Booking();
         booking.eventName = event._id;
-        booking.owner = event.owner.id;
+        booking.owner = event.owner._id;
         booking.booker = user._id;
 
         await booking.save();
       
-        event.save();
+        
         // Update the booked seats in the event
-        event.bookedSeats += 1;
+        event.bookedSeats += bookedSeats||1;
+        event.availableSeats -=bookedSeats||1;
         await event.save();
         return handleResponse(res, 200, booking,null, next);
     }
