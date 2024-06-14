1c3826dd39cd8f2799a9178c528c1d3cea150c2e
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-14 20:26:23 +0530

    add /

--- Routers/booking.router.js
+++ Routers/booking.router.js
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { bookerDetails } from "../controllers/bookingControllers/bookerDetails.controller.js";
 import { verifyToken } from "../middlewares/verifyJWT.middleware.js";
 import { bookSeat } from "../controllers/bookingControllers/bookSeat.controller.js";
+import { unbookSeat } from "../controllers/bookingControllers/unbookSeat.controller.js";
 
-const router =Router();
+const router = Router();
 
-router.get('/booker-details/:usesrname',verifyToken,bookerDetails);
-router.post('/booke-event',verifyToken,bookSeat);
-
+router.get('/booker-details/:usesrname', verifyToken, bookerDetails);
+router.post('/book-event/', verifyToken, bookSeat);
+router.delete('/unbooking-event/', verifyToken, unbookSeat);
 
 export default router;
