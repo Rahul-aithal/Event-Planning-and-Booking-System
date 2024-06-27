308d9286d3e072d3e8981525247e6e6290eb49d2
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-27 21:16:01 +0530

    added div

--- CLIENT/src/components/Footer/Footer.jsx
+++ CLIENT/src/components/Footer/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='bg-black text-white p-2 text-center position-relative bottom-0 w-100'>
+    <footer className=' position-relative bottom-0 w-100 h-100'>
+      <div className='bg-black text-white p-2 text-center '>
         <p>Discover, Book, and Celebrate with Us! Find the perfect events for every occasion, from parties to conferences, and make every moment unforgettable.</p>
         <p>&copy; 2024 Your Event Booking Website</p>
+        </div>
     </footer>
   )
 }
