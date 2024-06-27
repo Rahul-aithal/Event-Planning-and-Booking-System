e736f8e913880f94ce3094d2f15382f48d1a03d2
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-27 20:25:57 +0530

    added components

--- CLIENT/src/main.jsx
+++ CLIENT/src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import './index.css';
+import Layout from './Layout/Layout.jsx'
+import Login from './components/Login/Login.jsx'
+import Register from './components/Register/Register.jsx'
+import Home from './components/Home/Home.jsx'
+import Events_List from './components/Events_List/Events_List.jsx'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Layout />}>
+    <Route path='' element={<Home  />} />
+    <Route path='login' element={<Login />} />
+    <Route path='events' element={<Events_List />} />
+    <Route path='register' element={<Register />} />
+  </Route>
+)
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
