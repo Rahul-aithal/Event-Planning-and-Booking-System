ae215b59690706cdc32a5936b65a562446a5a7d7
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-27 20:24:28 +0530

    Create Footer

---
+++ CLIENT/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='bg-black text-white p-2 text-center position-relative bottom-0 w-100'>
+        <p>Discover, Book, and Celebrate with Us! Find the perfect events for every occasion, from parties to conferences, and make every moment unforgettable.</p>
+        <p>&copy; 2024 Your Event Booking Website</p>
+    </footer>
+  )
+}
+
+export default Footer
