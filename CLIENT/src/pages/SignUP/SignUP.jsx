631f821d30912e2663b98041e087ac17593aea19
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-30 22:08:55 +0530

    Signup component created

---
+++ CLIENT/src/pages/SignUP/SignUP.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SignUpComponent from '../../components/SignUpComponent';
+import Container from '../../components/container/Container';
+
+function SignUp() {
+  return (
+    <Container >
+     
+
+      <SignUpComponent />
+      
+    </Container>
+  );
+}
+
+export default SignUp;
