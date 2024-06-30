9e7e6b681adbbd1b5039590db2fe9bc6d927f658
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-30 22:14:49 +0530

    Contianer name component created and given styles to have everything in it

---
+++ CLIENT/src/components/container/Container.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import './container.css';
+import { useSelector } from 'react-redux';
+
+export default function Container({ children,className }) {
+  const theme = useSelector((state) => state.themeChanger.theme);
+  const dark = useSelector((state) => state.themeChanger.dark);
+  
+
+  useEffect(() => {
+    window.theme = theme;
+  }, [theme]);
+
+  return (
+    <div
+      className={`container-fluid py-5 px-auto ${dark?"bg-black text-light":"bg-light-subtle text-dark"} ${className}`}
+      id='Container'
+      data-bs-theme={theme}
+    >
+      {children}
+    </div>
+  );
+}
