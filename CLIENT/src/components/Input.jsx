04fdd73936a5e28870cdc9bcb09090ad5fe9edb0
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-30 22:16:13 +0530

    Input component created and styled as required

---
+++ CLIENT/src/components/Input.jsx
@@ -0,0 +1,24 @@
+import React, { forwardRef, useId } from 'react'
+
+const Input = forwardRef( function Input(
+{
+label,
+
+type="text",
+name=null,
+className,
+...props
+},
+ref
+) {
+     const id=useId()
+  return (
+    <div className="mb-3">
+    {label && <label htmlFor={id} className={`form-control border-0 bg-transparent fw-bolder`}>{label+":"}</label>}
+    <input type={type} id={id} name={name!==null?name:label.toLowerCase()} className={`form-control ${className}`}
+    {...props} />
+    </div>
+  )
+})
+
+export default Input
