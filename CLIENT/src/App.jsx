537e44bbcc77120912b9d39747061c85ff431a49
Rahul-aithal <aithalrahul34@gmail.com>
2024-06-30 22:05:39 +0530

    App creted and used as layout

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,12 +1,19 @@
-
+import { Outlet } from 'react-router-dom';
+import NavBar from './pages/Navbar/NavBar';
+import Footer from './pages/Footer/Footer';
+import React from 'react';
+import Container from './components/container/Container';
 
 function App() {
-
   return (
     <>
-   
+      <NavBar />
+      <Container >
+        <Outlet />
+      </Container>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
