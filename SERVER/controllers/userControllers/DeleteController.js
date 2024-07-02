869edcba253e434679272fccea94c85baa432929
Rahul-aithal <aithalrahul34@gmail.com>
2024-07-02 22:10:04 +0530

    added username and email checks

--- SERVER/controllers/userControllers/DeleteController.js
+++ SERVER/controllers/userControllers/DeleteController.js
@@ -1,33 +1,42 @@
 
 
 import { User } from "../../models/user.model.js";
-
-
+import { handleResponse } from "../../utils/HandleResponse.js";
+import { comparePassword } from "../../utils/HandlePassword.js";
 //@desc Delete User
 //@route  DELETE/api/user/
 export const DelteUser = async (req, res, next) => {
 
     try {
-        const { password } = req.body
+        const { username,email,password } = req.body
         const user = req.user
-        if (!user) {
-            return handleResponse(res, 404, _, new Error("User not found"), next);
+        if(!username&&!email&&!password){
+            return handleResponse(res, 402,{ username,email,password } , new Error("All Feilds are compulsory"), next);
 
+        }
+        if (!user) {
+            return handleResponse(res, 404, null, new Error("User not found"), next);
+
+        }
+        if(user.username!==username){
+            return handleResponse(res, 404, null, new Error("Invaild username"), next);
+        }
+        if(user.email!==email){
+            return handleResponse(res, 404, null, new Error("Invaild Email"), next);
         }
         const userPassword = user.password;
 
-
         if (comparePassword(password, userPassword)) {
 
-            const DelteUser = await User.findByIdAndDelete(user._id);
-            return handleResponse(res, 200, DelteUser, _, next);
+            const DelteUser = await User.findByIdAndDelete(user.nullid);
+            return handleResponse(res, 200, DelteUser, null, next);
         }
          else 
          {
-            return handleResponse(res, 401, _, new Error("Invalid password"), next);
+            return handleResponse(res, 401, null, new Error("Invalid password"), next);
         }
     }
     catch (error) {
-        return handleResponse(res, 500, _, error, next);
+        return handleResponse(res, 500, null, error, next);
     }
 }
