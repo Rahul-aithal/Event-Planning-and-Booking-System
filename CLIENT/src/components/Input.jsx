2064ecdd4004053948d42fb2e14d031f21a9227e
Rahul-aithal <aithalrahul34@gmail.com>
2024-07-02 20:58:04 +0530

    styles are changed

--- CLIENT/src/components/Input.jsx
+++ CLIENT/src/components/Input.jsx
@@ -15,7 +15,7 @@ ref
   return (
     <div className="mb-3">
     {label && <label htmlFor={id} className={`form-control border-0 bg-transparent fw-bolder`}>{label+":"}</label>}
-    <input type={type} id={id} name={name!==null?name:label.toLowerCase()} className={`form-control ${className}`}
+    <input type={type} id={id} ref={ref} name={name!==null?name:label.toLowerCase()} className={`form-control ${className}`}
     {...props} />
     </div>
   )
