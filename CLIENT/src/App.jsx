679e6d6997081f6562c3fb9736e1b297f55fa3fe
Rahul-aithal <aithalrahul34@gmail.com>
2024-07-02 20:54:07 +0530

    Bootstrap classes added

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,14 +1,15 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from './pages/Navbar/NavBar';
 import Footer from './pages/Footer/Footer';
-import React from 'react';
 import Container from './components/container/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <NavBar />
-      <Container >
+      <Container>
         <Outlet />
       </Container>
       <Footer />
