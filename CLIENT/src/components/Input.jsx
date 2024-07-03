8b6c47d36ab0041ba2cb0eddfea1573eca701469
Rahul-aithal <aithalrahul34@gmail.com>
2024-07-03 20:15:51 +0530

    indentation and id values changed

--- CLIENT/src/components/Input.jsx
+++ CLIENT/src/components/Input.jsx
@@ -1,22 +1,22 @@
 import React, { forwardRef, useId } from 'react'
 
-const Input = forwardRef( function Input(
-{
-label,
-
-type="text",
-name=null,
-className,
-...props
-},
-ref
+const Input = forwardRef(function Input(
+  {
+    label,
+    id = null,
+    type = "text",
+    name = null,
+    className,
+    ...props
+  },
+  ref
 ) {
-     const id=useId()
+  const randId = useId()
   return (
     <div className="mb-3">
-    {label && <label htmlFor={id} className={`form-control border-0 bg-transparent fw-bolder`}>{label+":"}</label>}
-    <input type={type} id={id} ref={ref} name={name!==null?name:label.toLowerCase()} className={`form-control ${className}`}
-    {...props} />
+      {label && <label htmlFor={id === null ? randId : id} className={`form-control border-0 bg-transparent fw-bolder`}>{label + ":"}</label>}
+      <input type={type} id={id === null ? randId : id} ref={ref} name={name !== null ? name : label.toLowerCase()} className={`form-control ${className}`}
+        {...props} />
     </div>
   )
 })
